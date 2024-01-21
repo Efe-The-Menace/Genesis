540c601136233236eb598a8574194d76a01d36e5
Efe-The-Menace <142423495+Efe-The-Menace@users.noreply.github.com>
2024-01-21 01:24:13 +0100

    Made changes to testfile.py

--- testfile.py
+++ testfile.py
@@ -1,4 +1,18 @@
-class employee:
-  def __innit__(self, name, age, department):
-    self.name = "name"
-    self.age = "age"
+class stafff:
+  def __innit__(self, first, last, age, department):
+    self.first = first
+    self.age = age
+    self.department = department
+    self.last = last
+
+    def intro(self):
+      return '{} {}'.format(self.first, self.last)
+    
+class Developer(stafff):
+  def __init__(self, first, last, department, pay, age, level, pro_lang):
+        super().__init__(first, last, age, department)
+        self.pro_lang = pro_lang
+        self.level = level
+        self.pay = pay
+    
+emp1 = stafff('david', 'oba', 20, 'HR')
