1027e8cb787f024be43f0ec4cfbf2446a700598b
Efe-The-Menace <142423495+Efe-The-Menace@users.noreply.github.com>
2024-01-21 01:38:56 +0100

    Another update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Genesis
 First time I created a GitHub repository.
+Was tough, not tough enough as you can see.
--- testfile.py
+++ testfile.py
@@ -1,18 +1,30 @@
-class stafff:
-  def __innit__(self, first, last, age, department):
-    self.first = first
-    self.age = age
-    self.department = department
-    self.last = last
-
-    def intro(self):
-      return '{} {}'.format(self.first, self.last)
-    
-class Developer(stafff):
-  def __init__(self, first, last, department, pay, age, level, pro_lang):
-        super().__init__(first, last, age, department)
-        self.pro_lang = pro_lang
-        self.level = level
+class Employee:
+    raise_amount = 1.09
+    def __init__(self, first, last, department, pay, level):
+        self.first = first
+        self.last = last
+        self.department = department
         self.pay = pay
+        self.level = level
+        self.email = first + last + '.' + '@menace.com'
+
+    def fullName(self):
+        return '{} {} {}'.format(self.first, self.last, self.raise_amount)
     
-emp1 = stafff('david', 'oba', 20, 'HR')
+class Developer(Employee):
+  def __init__(self, first, last, department, pay, level, pro_lang):
+        super().__init__(first, last, pay, level,  department)
+        self.pro_lang = pro_lang
+        
+class manager(Employee):
+    def __init__(self, first, last, department, pay, level, zone, employees = None):
+        super().__init__(first, last, department, pay, level)
+        self.zone = zone
+        self.employees = employees
+
+emp_1 = Employee('Efe', 'Obasuyi', 'Admin', 200000, 400)
+emp_2 = Developer('David', 'Obasuyi', 500, 'HR', 150000, 'Python')
+manager_1 = manager('Elon', 'Musk', 'Executive', 1000000, 1000, 'kaduna', ['David', 'John', 'Emmannuel','oghogho', 'Cathrine', 'Sandra'])
+'''print(Employee.raise_amount)
+print(manager_1.__dict__)'''
+
